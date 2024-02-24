008925a89682817e43fcbb17b44dc9fbd7d6e9cc
AluanLuiz <aluanluiz@outlook.com>
2024-02-24 12:09:35 -0300

    Teste de biblioteca Turtle

--- test_6.py
+++ test_6.py
@@ -3,21 +3,21 @@
 import turtle
 
 a = 0
-b = 1
+b = 0
 
 turtle.bgcolor("Black")
 turtle.speed(0)
 turtle.pencolor("Green")
 turtle.penup()
-turtle.goto(0, 300)
+turtle.goto(0, 200)
 turtle.pendown()
 
 while True:
     turtle.forward(a)
     turtle.right(b)
-    a += 2
-    b += 2
-    if b ==300:
+    a += 3
+    b += 1
+    if b == 200:
         break
     
 turtle.exitonclick()
